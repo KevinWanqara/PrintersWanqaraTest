6b1833eddcb5c40611825b1af23b65edbacd8c84
KevinWanqara <kloja@wanqara.com>
2026-03-18 17:50:35 -0500

    feat: Bump version name to 1.1.0 in build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = 30 //Android 11 Red Velvet Cake
         targetSdk = 35
         versionCode = 2
-        versionName = "1.0.1"
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     }
